01d33227343607934e14012bb75782fb54799c68
Etsuji Nakai <enakai@redhat.com>
2014-07-06 08:27:03 +0900

    fixing wrong indent, don't add dupulicate iptables entry

--- setup.sh
+++ setup.sh
@@ -20,24 +20,27 @@ function rdo_install {
     rm -f /usr/lib/systemd/system/mariadb.service
     cp /usr/lib/systemd/system/mysqld.service /usr/lib/systemd/system/mariadb.service
 
-     yum -y install http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-8.noarch.rpm
-     yum -y install openstack-packstack
-     yum -y install openstack-packstack-2013.2.1-0.36.dev1013.fc20.noarch
-     packstack --gen-answer-file=answers.txt
-     sed -i 's/CONFIG_PROVISION_DEMO=.*/CONFIG_PROVISION_DEMO=n/' answers.txt
-     sed -i 's/CONFIG_SWIFT_INSTALL=.*/CONFIG_SWIFT_INSTALL=n/' answers.txt
-     sed -i 's/CONFIG_NAGIOS_INSTALL=.*/CONFIG_NAGIOS_INSTALL=n/' answers.txt
-     sed -i 's/CONFIG_HEAT_INSTALL=.*/CONFIG_HEAT_INSTALL=y/' answers.txt
-     sed -i 's/CONFIG_HEAT_CLOUDWATCH_INSTALL=.*/CONFIG_HEAT_CLOUDWATCH_INSTALL=y/' answers.txt
-     sed -i 's/CONFIG_HEAT_CFN_INSTALL=.*/CONFIG_HEAT_CFN_INSTALL=y/' answers.txt
-     packstack --answer-file=answers.txt
+    yum -y install http://repos.fedorapeople.org/repos/openstack/openstack-havana/rdo-release-havana-8.noarch.rpm
+    yum -y install openstack-packstack
+    yum -y install openstack-packstack-2013.2.1-0.36.dev1013.fc20.noarch
+    packstack --gen-answer-file=answers.txt
+    sed -i 's/CONFIG_PROVISION_DEMO=.*/CONFIG_PROVISION_DEMO=n/' answers.txt
+    sed -i 's/CONFIG_SWIFT_INSTALL=.*/CONFIG_SWIFT_INSTALL=n/' answers.txt
+    sed -i 's/CONFIG_NAGIOS_INSTALL=.*/CONFIG_NAGIOS_INSTALL=n/' answers.txt
+    sed -i 's/CONFIG_HEAT_INSTALL=.*/CONFIG_HEAT_INSTALL=y/' answers.txt
+    sed -i 's/CONFIG_HEAT_CLOUDWATCH_INSTALL=.*/CONFIG_HEAT_CLOUDWATCH_INSTALL=y/' answers.txt
+    sed -i 's/CONFIG_HEAT_CFN_INSTALL=.*/CONFIG_HEAT_CFN_INSTALL=y/' answers.txt
+    packstack --answer-file=answers.txt
 
-     . ~/keystonerc_admin
-     heat-manage db_sync
-     # https://bugzilla.redhat.com/show_bug.cgi?id=1106394
-     openstack-config --set --existing /etc/heat/heat.conf ec2authtoken auth_uri http://127.0.0.1:5000/v2.0
+    . ~/keystonerc_admin
+    heat-manage db_sync
+    # https://bugzilla.redhat.com/show_bug.cgi?id=1106394
+    openstack-config --set --existing /etc/heat/heat.conf ec2authtoken auth_uri http://127.0.0.1:5000/v2.0
 
-     sed -i '/^:nova-filter-top - .*/a -A INPUT -p tcp -m multiport --dports 8000,8003 -m comment --comment "001 heat API" -j ACCEPT' /etc/sysconfig/iptables
+    heat_filter='-A INPUT -p tcp -m multiport --dports 8000,8003 -m comment --comment "001 heat API" -j ACCEPT'
+    if ! grep -q -- "$heat_filter" /etc/sysconfig/iptables; then
+         sed -i "/^:nova-filter-top - .*/a $heat_filter" /etc/sysconfig/iptables
+    fi
 
     if virsh net-info default | grep -q -E "Active: *yes"; then
         virsh net-destroy default
